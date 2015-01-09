279275d222bdf215f13e4c9fc30c6f502566ae17
zulldesign <admin@zulldesign.ml>
2015-01-09 11:34:59 +0800

    l

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-  echo 'Ini beneran pake PHP lho (in bahasa) <br/>';
+  echo 'Haha Ini beneran pake PHP lho (in bahasa) <br/>';
   echo 'Current PHP version: ' . phpversion();
   
   echo '<br/>Created by @tonidermawan';
