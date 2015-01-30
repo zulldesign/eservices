9d672013adcb06805260b64fa6fb8a07727937fa
zulldesign <admin@zulldesign.ml>
2015-01-30 18:57:38 +0800

    l

--- wp-content/plugins/chat_importer/chat_importer.php
+++ wp-content/plugins/chat_importer/chat_importer.php
@@ -8,9 +8,14 @@
     Author URI: https://www.apphb.com
     */
 	
+function chaimp_admin()
+	{
+    include('chat_import_admin.php');
+	}
+ 
 function chaimp_admin_actions()
-    {
-	add_options_page("Chat Product Display", "Chat Product Display", 1, "Chat Product Display", "chaimp_admin");
+	{
+    add_options_page("Chat Product Display", "Chat Product Display", 1, "Chat Product Display", "chaimp_admin");
 	}
  
 	add_action('admin_menu', 'chaimp_admin_actions');
