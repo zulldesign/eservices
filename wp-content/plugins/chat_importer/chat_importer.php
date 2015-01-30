c02355783e4326f80a634bebd7e13427200e2899
zulldesign <admin@zulldesign.ml>
2015-01-30 10:10:41 +0800

    l

---
+++ wp-content/plugins/chat_importer/chat_importer.php
@@ -0,0 +1,10 @@
+<?php 
+    /*
+    Plugin Name: Apphb Chat Display
+    Plugin URI: https://www.apphb.com
+    Description: Plugin for displaying Chat from an Apphb database
+    Author: Zulf
+    Version: 1.0
+    Author URI: https://www.apphb.com
+    */
+?>
