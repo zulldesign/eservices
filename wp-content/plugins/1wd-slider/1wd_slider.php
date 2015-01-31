8232d6442c5e7814439d5935651a7a84479e2932
zulldesign <admin@zulldesign.ml>
2015-01-31 16:09:08 +0800

    l

---
+++ wp-content/plugins/1wd-slider/1wd_slider.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: 1WD Slider
+Plugin URI: http://1stwebdesigner.com/
+Description: Slider Component for WordPress
+Version: 1.0
+Author: Rakhitha Nimesh
+Author URI: http://1stwebdesigner.com/
+License: GPLv2 or later
+*/
+
+function fwds_slider_activation() {
+}
+register_activation_hook(__FILE__, 'fwds_slider_activation');
+
+function fwds_slider_deactivation() {
+}
+register_deactivation_hook(__FILE__, 'fwds_slider_deactivation');
+
+
