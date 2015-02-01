0e80d300953f8f4578bfa7794fe976ebd26cd944
zulldesign <admin@zulldesign.ml>
2015-02-01 10:10:59 +0800

    l

--- wp-content/plugins/1wd-slider/1wd_slider.php
+++ wp-content/plugins/1wd-slider/1wd_slider.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: 1WD Slider
 Plugin URI: http://1stwebdesigner.com/
@@ -8,13 +9,57 @@ Author: Rakhitha Nimesh
 Author URI: http://1stwebdesigner.com/
 License: GPLv2 or later
 */
-
 function fwds_slider_activation() {
 }
 register_activation_hook(__FILE__, 'fwds_slider_activation');
+
 
 function fwds_slider_deactivation() {
 }
 register_deactivation_hook(__FILE__, 'fwds_slider_deactivation');
 
 
+
+
+add_action('wp_enqueue_scripts', 'fwds_scripts');
+function fwds_scripts() {
+
+  wp_enqueue_script('jquery');
+
+  wp_register_script('slidesjs_core', plugins_url('js/jquery.slides.min.js', __FILE__),array("jquery"));
+  wp_enqueue_script('slidesjs_core');
+
+  wp_register_script('slidesjs_init', plugins_url('js/slidesjs.initialize.js', __FILE__));
+  wp_enqueue_script('slidesjs_init');
+
+}
+
+
+add_action('wp_enqueue_scripts', 'fwds_styles');
+function fwds_styles() {
+
+  wp_register_style('slidesjs_example', plugins_url('css/example.css', __FILE__));
+  wp_enqueue_style('slidesjs_example');
+  wp_register_style('slidesjs_fonts', plugins_url('css/font-awesome.min.css', __FILE__));
+  wp_enqueue_style('slidesjs_fonts');
+
+}
+
+add_shortcode("1wd_slider", "fwds_display_slider");
+function fwds_display_slider() {
+
+  $plugins_url = plugins_url();
+ 
+
+  echo '<div class="container">
+    <div id="slides">
+      <img src="'.plugins_url( 'img/example-slide-1.jpg' , __FILE__ ).'" />
+      <img src="'.plugins_url( 'img/example-slide-2.jpg' , __FILE__ ).'" />
+      <img src="'.plugins_url( 'img/example-slide-3.jpg' , __FILE__ ).'" />
+      <img src="'.plugins_url( 'img/example-slide-4.jpg' , __FILE__ ).'" />
+      <a href="#" class="slidesjs-previous slidesjs-navigation"><i class="icon-chevron-left icon-large"></i></a>
+      <a href="#" class="slidesjs-next slidesjs-navigation"><i class="icon-chevron-right icon-large"></i></a>
+    </div>
+  </div>';
+}
+?>
---
+++ wp-content/plugins/1wd-slider/js/slidesjs.initialize.js
@@ -0,0 +1,8 @@
+jQuery(function() {
+      jQuery('#slides').slidesjs({
+        width: 940,
+        height: 528,
+        navigation: false
+      });
+    });
+
